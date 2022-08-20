88503ff15b2aecc8f8000d341a0347d74850b4bf
Deltora <deltora1st@gmail.com>
2022-08-20 20:58:31 +0700

    signup page

--- client/src/components/homepage/auth/signup.jsx
+++ client/src/components/homepage/auth/signup.jsx
@@ -3,58 +3,59 @@ import "../../../styles/signup.css";
 
 export default function Signup() {
     return(
-        <div class="wrapper">
-      <div class="ctr">
-        <div class="ctr_icon">
-          <i class="fa-solid fa-x fa-2x ctr_icon_my-icon"></i>
-        </div>
-        <h1 class="ctr_header">Welcome back</h1>
-        <p class="ctr_para">Join us to savor good things in this life</p>
-        <form method="post" class="ctr_form">
-          <div class="ctr_form_text-field">
-            <input class="ctr_form_text-field_input" type="text" />
-            <label class="ctr_form_text-field_label">Full name</label>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+          <link rel="stylesheet" href="signup_main.css" />
+          <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet' />
+          <title>Sign up</title>
+        </head>
+        <body>
+          <div class="wrapper">
+            <div class="ctr">
+              <div class="ctr_icon">
+                <i class="fa-solid fa-x fa-2x ctr_icon_my-icon"></i>
+              </div>
+              <h1 class="ctr_header">Welcome back</h1>
+              <p class="ctr_para">Join us to savor good things in this life</p>
+              <form method="post" class="ctr_form">
+                <div class="ctr_form_text-field">
+                  <input class="ctr_form_text-field_input" type="text" />
+                  <label class="ctr_form_text-field_label">Full name</label>
+                </div>
+                <div class="ctr_form_text-field">
+                  <input class="ctr_form_text-field_input" type="text" autocomplete="off" />
+                  <label class="ctr_form_text-field_label">User name</label>
+                </div>
+                <div class="ctr_form_text-field">
+                  <input class="ctr_form_text-field_input" type="text" autocomplete="off" required />
+                  <label class="ctr_form_text-field_label">Email</label>
+                </div>
+                <div class="ctr_form_text-field">
+                  <input class="ctr_form_text-field_input" type="password" autocomplete="off" required />
+                  <label class="ctr_form_text-field_label">Password</label>
+                </div>
+                <div class="ctr_form_text-field">
+                  <input class="ctr_form_text-field_input" type="password" autocomplete="off" required />
+                  <label class="ctr_form_text-field_label">Confirm password</label>
+                </div>
+                <div class="ctr_desc">
+                  By signing up, you agree to Heifurdesz's <a class="ctr_link" href="#">Term of Use </a>and <a class="ctr_link" href="#">Privacy Policy</a>.
+                </div>
+                <div class="ctr_sign-up-btn">
+                  <input type="submit" value="Sign up" class="ctr_sign-up-btn_submit" />
+                  <i class="fa-solid fa-arrow-right-long fa-2x ctr_sign-up-btn_icon"></i>
+                </div>
+                <div class="ctr_signup-link">
+                  Already have an account? <a class="ctr_link" href="#"><u>Log in</u></a>
+                </div>
+              </form>
+            </div>
           </div>
-          <div class="ctr_form_text-field">
-            <input class="ctr_form_text-field_input" type="text" autocomplete="off" />
-            <label class="ctr_form_text-field_label">User name</label>
-          </div>
-          <div class="ctr_form_text-field">
-            <input class="ctr_form_text-field_input" type="text" autocomplete="off" required />
-            <label class="ctr_form_text-field_label">Email</label>
-          </div>
-          <div class="ctr_form_text-field">
-            <input class="ctr_form_text-field_input" type="password" autocomplete="off" required />
-            <label class="ctr_form_text-field_label">Password</label>
-          </div>
-          <div class="ctr_form_text-field">
-            <input class="ctr_form_text-field_input" type="password" autocomplete="off" required />
-            <label class="ctr_form_text-field_label">Confirm password</label>
-          </div>
-          <div class="ctr_desc">
-            By signing up, you agree to Heifurdesz's <a class="ctr_link" href="#">Term of Use</a>
-            and <a class="ctr_link" href="#">Privacy Policy</a>.
-          </div>
-          <div class="ctr_sign-up-btn">
-            <input type="submit" value="Sign up" class="ctr_sign-up-btn_submit" />
-            <i class="fa-solid fa-arrow-right-long fa-2x ctr_sign-up-btn_icon"></i>
-          </div>
-          <div class="ctr_signup-link">
-            Already have an account? <a class="ctr_link" href="#"><u>Log in</u></a>
-          </div>
-        </form>
-      </div>
-    </div>
-    // <script>
-    //   document.querySelectorAll(".ctr_form_text-field_input").forEach((element) => {
-    //     element.addEventListener("blur", (event) => {
-    //         if (event.target.value != "") {
-    //             event.target.nextElementSibling.classList.add("ctr_form_filled");
-    //         } else {
-    //             event.target.nextElementSibling.classList.remove("ctr_form_filled");
-    //         }
-    //     });
-    // });
-    // </script>
+        </body>
+      </html>
     )
 }
--- client/src/components/homepage/homepage.jsx
+++ client/src/components/homepage/homepage.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Login from "./auth/login";
 import Signup from "./auth/signup";
 
 export default function Homepage() {
+    useEffect(() => {
+        document.querySelectorAll(".ctr_form_text-field_input").forEach((element) => {
+            element.addEventListener("blur", (event) => {
+                if (event.target.value != "") {
+                    event.target.nextElementSibling.classList.add("ctr_form_filled");
+                } else {
+                    event.target.nextElementSibling.classList.remove("ctr_form_filled");
+                }
+            });
+        });
+    });
     return(
         <div>
             <Login />
