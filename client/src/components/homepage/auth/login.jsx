722e512796ff4e1d94cbf9a2b622e5a8d09f6aa5
NHuyHoangg <80319763+NHuyHoangg@users.noreply.github.com>
2022-08-20 15:25:56 +0700

    login not show up

--- client/src/components/homepage/auth/login.jsx
+++ client/src/components/homepage/auth/login.jsx
@@ -1,8 +1,53 @@
 import React from "react";
 import "../../../styles/login.css";
 
-export default function Login() {
-    return(
-        <h1>Built Login here</h1>
-    )
+
+export default function Login(props) {
+  // return(
+  return (props.trigger) ? (
+    <div className="wrapper_body">
+    <div className="wrapper">
+      <div className="ctr">
+        <div className="ctr_icon">
+          <button className="ctr_close-btn" onClick={()=> props.setTrigger(false)}> <i className="fa-solid fa-x fa-2x ctr_icon_my-icon"></i></button>
+        </div>
+        <h1 className="ctr_header">Welcome back</h1>
+        <p className="ctr_para">Join us to savor good things in this life</p>
+        <form method="post" className="ctr_form">
+          <div className="ctr_form_text-field">
+            <input className="ctr_form_text-field_input" type="text" autocomplete="off" required />
+            <label className="ctr_form_text-field_label">Email</label>
+          </div>
+          <div className="ctr_form_text-field">
+            <input className="ctr_form_text-field_input" type="password" autocomplete="off" required />
+            <label className="ctr_form_text-field_label">Password</label>
+          </div>
+          <div className="ctr_desc">
+            <a className="ctr_link" href="#" style="float:right">Forgot Password?</a>
+          </div>
+          <div className="ctr_sign-up-btn">
+            <button className="ctr_sign-up-btn_submit" type="submit" >Enjoy now !</button>
+            <i className="fa-solid fa-arrow-right-long fa-2x ctr_sign-up-btn_icon"></i>
+          </div>
+          <div className="ctr_signup-link">
+            New to Heifurdesz? <a className="ctr_link" href="#"><u>Sign up</u></a>
+          </div>
+        </form>
+      </div>
+      </div>
+      </div>
+    // <script type="text/javascript" >
+    //   document.querySelectorAll(".ctr_form_text-field_input").forEach((element) => {
+    //     element.addEventListener("blur", (event) => {
+    //       if (event.target.value != "") {
+    //         event.target.nextElementSibling.classList.add("ctr_form_filled");
+    //       } else {
+    //         event.target.nextElementSibling.classList.remove("ctr_form_filled");
+    //       }
+    //     });
+    //   });
+    // </script>
+  ) : "";
+  // )
 }
+
--- client/src/components/homepage/homepage.jsx
+++ client/src/components/homepage/homepage.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Login from "./auth/login";
 import Signup from "./auth/signup";
+import { useState, useEffect} from "react";
 
 export default function Homepage() {
-    return(
-        <div>
-            <Login />
-            <Signup />
-        </div>
-    )
+    const [buttonLogin, setButtonLogin] = useState(false);
+    return (
+      <div>
+        <button
+          className="ctr_sign-up-btn_submit"
+          onClick={() => setButtonLogin(true)}>
+          Login
+        </button>
+        <Login trigger={buttonLogin} setTrigger={setButtonLogin}></Login>
+        {/* <Login/> */}
+        <Signup />
+      </div>
+    );
 }
