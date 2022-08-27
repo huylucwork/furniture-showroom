dcd9cbf8bd94a5fcdea482fea2c9177c4fe904b7
Deltora <deltora1st@gmail.com>
2022-08-28 00:37:07 +0700

    new version

--- client/src/user/history.jsx
+++ client/src/user/history.jsx
@@ -6,9 +6,20 @@ export default function History() {
       <div className="history_container">
         <div className="history_navbar"></div>
         <div className="history_sidebar">
-          <button className="history_user-profile" type="button">
-            User Profile
-          </button>
+          <div className="history_sidebar_container">
+            <button className="history_user-profile" type="button">
+              User Profile
+            </button>
+            <i class="fa-solid fa-user history_user-icon-profile"></i>
+          </div>
+          <div className="history_sidebar_container">
+            <button className="history_user-history" type="button">
+              History
+            </button>
+            {/* <i class="fa-thin fa-grid-2-plus history_user-icon-history"></i> */}
+            <i class="fa-thin fa-closed-captioning history_user-icon-history"></i>
+            {/* <i class="fa-thin fa-grid-2 history_user-icon-history"></i> */}
+          </div>
         </div>
       </div>
     )
