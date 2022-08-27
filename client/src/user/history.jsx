a05ca89a9b7dec3083aa3da6ba54574e048787bf
Deltora <deltora1st@gmail.com>
2022-08-27 21:13:03 +0700

    ver1

--- client/src/user/history.jsx
+++ client/src/user/history.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import "../../../styles/history.css";
+import "../styles/history.css";
 
 export default function History() {
     return(
       <div className="history_container">
-        
       </div>
     )
 }
