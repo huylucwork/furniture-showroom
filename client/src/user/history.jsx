349935af57b3414223aca28d67bfe99926946098
Deltora <deltora1st@gmail.com>
2022-08-27 23:43:07 +0700

    version2

--- client/src/user/history.jsx
+++ client/src/user/history.jsx
@@ -4,6 +4,12 @@ import "../styles/history.css";
 export default function History() {
     return(
       <div className="history_container">
+        <div className="history_navbar"></div>
+        <div className="history_sidebar">
+          <button className="history_user-profile" type="button">
+            User Profile
+          </button>
+        </div>
       </div>
     )
 }
