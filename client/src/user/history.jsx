d14b9a2e6b5dc603d3db6994428e6fdf433b4068
Deltora <deltora1st@gmail.com>
2022-08-29 00:49:11 +0700

    new version

--- client/src/user/history.jsx
+++ client/src/user/history.jsx
@@ -13,7 +13,7 @@ export default function History() {
           <i className="fa-solid fa-user history_user-icon-profile"></i>
         </div>
         <div className="history_sidebar_container">
-          <button className="history_user-history" type="button" onclick="myFunction()" id="myDIV">
+          <button className="history_user-history" type="button">
             History
           </button>
           <i className="fa-solid fa-clock-rotate-left history_user-icon-history"></i>
@@ -24,7 +24,175 @@ export default function History() {
           <input className="history_search" type="text" placeholder="Search..." />
           <i class="fa-solid fa-magnifying-glass history_search-icon"></i>       
         </div>
-        <div className="history_table"></div>
+        <div className="history_table">
+          <div className="history_table-row history_table-first">
+            <div className="history_table-element">
+              <p className="history_table-head">Date</p>
+              <button className="history_head-btn"></button>
+              <i class="fa-solid fa-up-down history_head-icon"></i>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table-head">Order code</p>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table-head">Total amount</p>
+              <button className="history_head-btn"></button>
+              <i class="fa-solid fa-up-down history_head-icon"></i>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table-head">Total payment</p>
+              <button className="history_head-btn"></button>
+              <i class="fa-solid fa-up-down history_head-icon"></i>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table-head">Export order</p>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table-head">Detail</p>
+            </div>
+          </div>
+          <div className="history_table-row history_table-odd">
+            <div className="history_table-element">
+              <p className="history_table_text">dd/mm/yy</p>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table_text">SO4xxxxxxxxx</p>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table_text">xxx</p>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table_text">$xxx</p>
+            </div>
+            <div className="history_table-element">
+              <button class="history_table-btn history_table_overwrite-bgr"><i class="fa-regular fa-circle-down history_download-icon"></i></button>
+            </div>
+            <div className="history_table-element">
+              <button className="history_detail-btn">Detail</button>
+            </div>
+          </div>
+          <div className="history_table-row history_table-even">
+            <div className="history_table-element">
+              <p className="history_table_text">dd/mm/yy</p>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table_text">SO4xxxxxxxxx</p>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table_text">xxx</p>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table_text">$xxx</p>
+            </div>
+            <div className="history_table-element">
+              <button className="history_table-btn"><i class="fa-regular fa-circle-down history_download-icon"></i></button>
+            </div>
+            <div className="history_table-element">
+              <button className="history_detail-btn">Detail</button>
+            </div>
+          </div>
+          <div className="history_table-row history_table-odd">
+            <div className="history_table-element">
+              <p className="history_table_text">dd/mm/yy</p>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table_text">SO4xxxxxxxxx</p>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table_text">xxx</p>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table_text">$xxx</p>
+            </div>
+            <div className="history_table-element">
+              <button className="history_table-btn history_table_overwrite-bgr"><i class="fa-regular fa-circle-down history_download-icon"></i></button>
+            </div>
+            <div className="history_table-element">
+              <button className="history_detail-btn">Detail</button>
+            </div>
+          </div>
+          <div className="history_table-row history_table-even">
+            <div className="history_table-element">
+              <p className="history_table_text">dd/mm/yy</p>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table_text">SO4xxxxxxxxx</p>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table_text">xxx</p>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table_text">$xxx</p>
+            </div>
+            <div className="history_table-element">
+              <button className="history_table-btn"><i class="fa-regular fa-circle-down history_download-icon"></i></button>
+            </div>
+            <div className="history_table-element">
+              <button className="history_detail-btn">Detail</button>
+            </div>
+          </div>
+          <div className="history_table-row history_table-odd">
+            <div className="history_table-element">
+              <p className="history_table_text">dd/mm/yy</p>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table_text">SO4xxxxxxxxx</p>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table_text">xxx</p>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table_text">$xxx</p>
+            </div>
+            <div className="history_table-element">
+              <button className="history_table-btn history_table_overwrite-bgr"><i class="fa-regular fa-circle-down history_download-icon"></i></button>
+            </div>
+            <div className="history_table-element">
+              <button className="history_detail-btn">Detail</button>
+            </div>
+          </div>
+          <div className="history_table-row history_table-even">
+            <div className="history_table-element">
+              <p className="history_table_text">dd/mm/yy</p>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table_text">SO4xxxxxxxxx</p>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table_text">xxx</p>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table_text">$xxx</p>
+            </div>
+            <div className="history_table-element">
+              <button className="history_table-btn"><i class="fa-regular fa-circle-down history_download-icon"></i></button>
+            </div>
+            <div className="history_table-element">
+              <button className="history_detail-btn">Detail</button>
+            </div>
+          </div>
+          <div className="history_table-row history_table-odd">
+            <div className="history_table-element">
+              <p className="history_table_text">dd/mm/yy</p>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table_text">SO4xxxxxxxxx</p>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table_text">xxx</p>
+            </div>
+            <div className="history_table-element">
+              <p className="history_table_text">$xxx</p>
+            </div>
+            <div className="history_table-element">
+              <button className="history_table-btn history_table_overwrite-bgr"><i class="fa-regular fa-circle-down history_download-icon"></i></button>
+            </div>
+            <div className="history_table-element">
+              <button className="history_detail-btn">Detail</button>
+            </div>
+          </div>
+          <div className="history_table-row"></div>
+        </div>
       </div>
     </div>
   )
