213b6edcd1b0c882487e97ee8e9cbb44b8da2649
Deltora <deltora1st@gmail.com>
2022-08-29 22:38:49 +0700

    add history2

--- client/src/user/history.jsx
+++ client/src/user/history.jsx
@@ -3,8 +3,81 @@ import React from "react";
 import "../styles/history.css";
 
 export default function History() {
+  // useEffect(() => {
+  //   var history_open_modal = document.querySelectorAll('.history_detail-btn')
+  //   var history_icon_close = document.querySelectorAll('.history_detail_close-btn')
+  //   var history_modal = document.querySelectorAll('.history_order_detail')
+
+  //   function history_toggleModal() {
+  //     history_modal.classList.toggle('history_hide-modal')
+  //   }
+
+  //   history_open_modal.addEventListener('click', history_toggleModal)
+  // });
   return(
     <div className="history_container">
+      <div className="history_order_detail history_hide-modal">
+        <div className="history_order_detail_wrapper">
+          <div className="history_detail_ctn">
+            <div className="history_detail_info">
+              <div className="history_detail_user-info">
+                <p className="history_info_header">Order: S04xxxxxxxxx</p>
+                <p className="history_info_text">Date: DD-MM-YYYY</p>
+                <p className="history_info_text">NAME: XXXX XXXX XXXX</p>
+                <p className="history_info_text">Email: example@email.com</p>
+                <p className="history_info_text">Phone: 0123xxxxxx</p>
+                <p className="history_info_text">Adress: xxx yyyyy str, xxxxxx, yyyyy, zzzzz</p>
+              </div>
+              <div className="history_detail_total">
+                <p className="history_info_text-total">Total:</p>
+                <p className="history_info_number-total">$1000000</p>
+              </div>
+            </div>
+            <div className="history_detail_item">
+              <i className="fa-solid fa-xmark history_detail_close-btn"></i>
+              <div className="history_detail_item-ctn">
+                <div className="history_detail_item-info">
+                  <div className="history_detail_item-img"></div>
+                  <div className="history_detail_item-name-cat">
+                    <p className="history_detail_item-name">Item X</p>
+                    <p className="history_detail_item-cat">Category</p>
+                  </div>
+                  <p className="history_detail_amount">Amount <br></br> 1</p>
+                  <p className="history_detail_item-price">Price <br></br> $123</p>
+                </div>
+                <div className="history_detail_item-info">
+                  <div className="history_detail_item-img"></div>
+                  <div className="history_detail_item-name-cat">
+                    <p className="history_detail_item-name">Item X</p>
+                    <p className="history_detail_item-cat">Category</p>
+                  </div>
+                  <p className="history_detail_amount">Amount <br></br> 1</p>
+                  <p className="history_detail_item-price">Price <br></br> $123</p>
+                </div>
+                <div className="history_detail_item-info">
+                  <div className="history_detail_item-img"></div>
+                  <div className="history_detail_item-name-cat">
+                    <p className="history_detail_item-name">Item X</p>
+                    <p className="history_detail_item-cat">Category</p>
+                  </div>
+                  <p className="history_detail_amount">Amount <br></br> 1</p>
+                  <p className="history_detail_item-price">Price <br></br> $123</p>
+                </div>
+                <div className="history_detail_item-info">
+                  <div className="history_detail_item-img"></div>
+                  <div className="history_detail_item-name-cat">
+                    <p className="history_detail_item-name">Item X</p>
+                    <p className="history_detail_item-cat">Category</p>
+                  </div>
+                  <p className="history_detail_amount">Amount <br></br> 1</p>
+                  <p className="history_detail_item-price">Price <br></br> $123</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="history_navbar"></div>
       <div className="history_sidebar">
         <div className="history_sidebar_container">
           <button className="history_user-profile" type="button">
@@ -191,7 +264,25 @@ export default function History() {
               <button className="history_detail-btn">Detail</button>
             </div>
           </div>
-          <div className="history_table-row"></div>
+          <div className="history_table-row">
+            <div className="history_pagination">
+              <a className="history_pagination_link history_pagination-text" href="#">
+                <p>Previous</p>
+              </a>
+              <a className="history_pagination_link" href="#">
+                <p>1</p>
+              </a>
+              <a className="history_pagination_link" href="#">
+                <p>2</p>
+              </a>
+              <a className="history_pagination_link" href="#">
+                <p>3</p>
+              </a>
+              <a className="history_pagination_link history_pagination-text" href="#">
+                <p>Next</p>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
