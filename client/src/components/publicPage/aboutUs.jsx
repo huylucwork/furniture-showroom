443b01e8470a037d8e037e9a742da46af6631c5f
Nhat Ha <ng.nhatha32@gmail.com>
2022-11-20 05:09:35 +0700

    about us

--- client/src/components/publicPage/aboutUs.jsx
+++ client/src/components/publicPage/aboutUs.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import Iframe from "react-iframe";
 import "../../styles/aboutUs.css";
 
 export default function AboutUs() {
   return(
     <div class="aboutUs_container">
+        <div className="vid">
+            <Iframe 
+            width="100%" height="100%" 
+            src="https://www.youtube.com/embed/S-A0qLTxf-U?autoplay=1" 
+            title="Home Furniture Ad Video Template (Editable)" 
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen />
+        </div>
         <section class="aboutUs_navbar"></section>
         <section class="aboutUs_intro-vid">
             <h1>HIFURDEZ</h1>
