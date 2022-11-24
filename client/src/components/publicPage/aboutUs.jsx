07031f5cf65f8894d01e502120cafd6eed80a98f
Nhat Ha <ng.nhatha32@gmail.com>
2022-11-24 16:50:13 +0700

    css text area

--- client/src/components/publicPage/aboutUs.jsx
+++ client/src/components/publicPage/aboutUs.jsx
@@ -54,7 +54,7 @@ export default function AboutUs() {
                         </p>
                     </ul>
                 </div>
-                <textarea name="contact" className="aboutUs_contact-info_sth"></textarea>
+                <textarea name="contact" className="aboutUs_contact-info_sth" placeholder="Send we an email!"></textarea>
                 <input type="submit" value="Submit" className="aboutUs_contact_submit"></input>
                 {/* <img src="#" alt="#" /> */}
                 <Iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1234.5304992677686!2d106.65528566907425!3d10.781406281320905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752ec864b87985%3A0x1a24a166991dec45!2zMjY4IEzDvSBUaMaw4budbmcgS2nhu4d0LCBQaMaw4budbmcgNiwgVMOibiBCw6xuaCwgVGjDoG5oIHBo4buRIEjhu5MgQ2jDrSBNaW5oLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1669106909469!5m2!1svi!2s" 
