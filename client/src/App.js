ad307a1aa82ab2f941a6c419a50cea90a58e46bc
Nhat Ha <ng.nhatha32@gmail.com>
2022-11-25 15:10:07 +0700

    alert

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import Axios from "axios";
+import Alert from "./components/helper/alert";
 import Header from "./components/headerFooter/header";
 import Homepage from "./components/publicPage/homepage";
 import User from "./components/user/user";
@@ -27,6 +28,9 @@ function App() {
   //filter
   const [filterCollection, setFilterCollection] = useState(collectionProduct[0]);
   const [filterCategory, setFilterCategory] = useState(categoryProduct[0]);
+
+  //alert
+  const [openAlert, setOpenAlert] = useState(true)
 
   return (
     <AppContext.Provider>
@@ -73,6 +77,7 @@ function App() {
           <Route path="admin/warehouse" element={<Admin tab={"warehouse"} />} />
           <Route path="admin/report" element={<Admin tab={"report"} />} />
           <Route path="admin" element={<Navigate to="manage-users" />} />
+          <Route path="alert" element={<Alert alert={{type: 'warning', message: 'gà'}} setOpenAlert={setOpenAlert} />} />
           <Route path="checkout" element={<Checkout setFilterCollection = {setFilterCollection} setFilterCategory = {setFilterCategory}/>} />
         </Routes>
         <Footer />
---
+++ client/src/components/helper/alert.jsx
@@ -0,0 +1,38 @@
+import "../../styles/alert.css"
+
+export default function Alert({ alert, setOpenAlert }) {
+
+    if (alert &&
+        (
+            alert.type === "error"
+            || alert.type === "success"
+            || alert.type === "warning"
+        )
+    ) {
+        return (
+            <div className={'alert_container ' + alert.type}>
+                <div className='alert_icon'>
+                    {alert.type === 'error' && <i className="fa-solid fa-circle-exclamation"></i>}
+                    {alert.type === 'success' && <i className="fa-solid fa-circle-check"></i>}
+                    {alert.type === 'warning' && <i className="fa-solid fa-triangle-exclamation"></i>}
+                </div>
+                <div className='alert_content'>
+                    <h3 className='alert_content_title'>
+                        {alert.type.charAt(0).toUpperCase() + alert.type.slice(1)}
+                    </h3>
+                    <p className='alert_content_message'>
+                        {alert.message}
+                    </p>
+                </div>
+                <div className="hidden">
+                {
+                    setTimeout(function () {
+                        setOpenAlert(false)
+                    }, 1300)
+                }
+                </div>
+            </div>
+
+        )
+    }
+}
