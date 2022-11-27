3353594ae35da74daa9413bcf71d4f1f67dd590b
Nhat Ha <ng.nhatha32@gmail.com>
2022-11-27 19:55:53 +0700

    fix merge

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ import Footer from "./components/headerFooter/footer";
 import Admin from "./components/admin/admin";
 import Checkout from "./components/cart/checkout";
 import ProductDetail from "./components/publicPage/productDetail";
-import Error from "./components/publicPage/error";
+import Error from "./components/helper/error";
 import { Analytics } from "@vercel/analytics/react";
 
 export const AppContext = React.createContext();
